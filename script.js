b1b8932da0f8ca441fa3777463ecb080e7055e6f
Dania <dania.liu@ocsbstudent.ca>
2021-03-17 15:12:59 +0000

    Took off whitespace

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.body.style.backgroundColor = 'red'
 
 // Adds a submission button, click to submit.
-document.getElementById('button').addEventListener ('click', results)
+document.getElementById('button').addEventListener('click', results)
 
 // Gives an alert when the textbox answer has been submitted.
 function results () {
