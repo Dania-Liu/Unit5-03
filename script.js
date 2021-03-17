f4196d5a628f7c69bd083f3dc3ec4c09d0a34560
Dania <dania.liu@ocsbstudent.ca>
2021-03-17 13:50:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//Adds a background colour.
+document.body.style.backgroundColor = "red";
+
+//Adds a submission button, click to submit.
+document.getElementById('button').addEventListener ('click', results)
+
+//Gives an alert when the textbox answer has been submitted.
+function results () {
+  alert(document.getElementById('bar').value)
+}
